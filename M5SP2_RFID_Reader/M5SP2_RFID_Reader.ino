2f35c7ab6202cf1dcae7c07496c7d4a7bfaa6c7d
ily-soft <ily.meftah@gmail.com>
2024-03-17 12:56:00 +0100

    update code M5SP2 RFID Reader

--- M5SP2_RFID_Reader/M5SP2_RFID_Reader.ino
+++ M5SP2_RFID_Reader/M5SP2_RFID_Reader.ino
@@ -3,7 +3,7 @@
 #include "MFRC522_I2C.h"
 
 #define MFRC522_I2C_ADDRESS 0x28
-#define RST_PIN -1 // Utilisez -1 si la broche de réinitialisation n'est pas utilisée
+#define RST_PIN -1
 
 MFRC522_I2C mfrc522(MFRC522_I2C_ADDRESS, RST_PIN, &Wire);
 
@@ -11,26 +11,48 @@ void setup() {
   M5.begin();
   Wire.begin();
   mfrc522.PCD_Init();
-  
-  // Définir l'orientation de l'écran. Remplacez '1' par 0, 1, 2, ou 3 selon l'orientation souhaitée.
-  M5.Lcd.setRotation(1); 
-  
+  M5.Lcd.setRotation(1);
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.setTextColor(WHITE);
   M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.println("RFID Reader");
+  M5.Lcd.println("Approchez un badge");
 }
 
 void loop() {
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
     M5.Lcd.fillScreen(BLACK);
     M5.Lcd.setCursor(0, 0);
-    M5.Lcd.print("UID:");
+    M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.print("UID: ");
+
+    String currentUID = "";
     for (byte i = 0; i < mfrc522.uid.size; i++) {
-      if(mfrc522.uid.uidByte[i] < 0x10) M5.Lcd.print(" 0");
-      else M5.Lcd.print(" ");
-      M5.Lcd.print(mfrc522.uid.uidByte[i], HEX);
+      if (mfrc522.uid.uidByte[i] < 0x10) {
+        currentUID += "0";
+      }
+      currentUID += String(mfrc522.uid.uidByte[i], HEX);
+      M5.Lcd.print(currentUID.substring(i * 2, i * 2 + 2));
+      if (i != mfrc522.uid.size - 1) {
+        M5.Lcd.print(" ");
+      }
     }
+
+    M5.Lcd.setCursor(0, 20);
+    M5.Lcd.print("Type: ");
+    String cardType = mfrc522.PICC_GetTypeName(mfrc522.PICC_GetType(mfrc522.uid.sak));
+    M5.Lcd.println(cardType);
+
+    M5.Lcd.setCursor(0, 40);
+    M5.Lcd.print("Taille: ");
+    byte cardSize = mfrc522.uid.size;
+    M5.Lcd.print(cardSize);
+    M5.Lcd.println(" octets");
+
     delay(2000);
+    mfrc522.PICC_HaltA();
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.println("Approchez un badge");
   }
 }
